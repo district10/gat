2b7473420a3906f989c59969a1c9c5037a59f9a6
Gnat TANG <gnat_tang@yeah.net>
2014-04-27 20:01:57 +0800

    good

--- gat-eim-SAMPLE.el
+++ gat-eim-SAMPLE.el
@@ -1,15 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/data/gat-tables/")
-
 (require 'quail)
 (require 'cl)
 
 ;; Define Package
 (quail-define-package
  "gat-input-method" "Chinese" "Gat_Zh_")
-
-;; Load Char Tables
-;; (require 'table-0001-1000) ;; Most Common Zh Chars
-;; (load "table-0001-6000")
 
 (quail-define-rules
  ("i" ?的)
